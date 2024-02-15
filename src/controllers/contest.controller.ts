5614314be4cc973161e132eb469b10d63a691fea
as6325400 <as6325400@gmail.com>
2024-02-16 02:01:00 +0800

    fix change

--- src/controllers/contest.controller.ts
+++ src/controllers/contest.controller.ts
@@ -1,21 +1,21 @@
 import { Request, Response } from "express";
 import database from "../config/database";
 
-interface Iaccepted{
-  problemName?:string,
-  teamName: string,
-  // 都沒 ac 過就是 undefined
-  acceptTime: number | undefined,
-  // try times
-  submissionTimes: number
-}
+// interface Iaccepted{
+//   problemName?:string,
+//   teamName: string,
+//   // 都沒 ac 過就是 undefined
+//   acceptTime: number | undefined,
+//   // try times
+//   submissionTimes: number
+// }
 
-interface Iproblem{
-  problemId: number,
-  problemName :string,
-  // 依照 acceptTime 由小到大排列
-  acceptTeam: Iaccepted[]
-}
+// interface Iproblem{
+//   problemId: number,
+//   problemName :string,
+//   // 依照 acceptTime 由小到大排列
+//   acceptTeam: Iaccepted[]
+// }
 
 interface Iteam{
   teamId: string,
@@ -24,14 +24,14 @@ interface Iteam{
 }
 
 
-interface Icontest{
-  contestId: number,
-  contestName: string,
-  Problems: Iproblem[],
-  // 有 submission 至少一題的隊伍數
-  teamNum: number,
-  teams: Iteam[]
-}
+// interface Icontest{
+//   contestId: number,
+//   contestName: string,
+//   Problems: Iproblem[],
+//   // 有 submission 至少一題的隊伍數
+//   teamNum: number,
+//   teams: Iteam[]
+// }
 
 class ContestController {
   private async getContestName(CID: number): Promise<string | undefined> {
